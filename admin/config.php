0a7057443f863b206e3a572dad3de09e84e9ae35
vitto-moz <v.mozharovsky@gmail.com>
2016-10-14 23:52:21 +0300

    blank opencart

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dobriruki.ua/admin/');
+define('HTTP_CATALOG', 'http://dobriruki.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dobriruki.ua/admin/');
+define('HTTPS_CATALOG', 'http://dobriruki.ua/');
+
+// DIR
+define('DIR_APPLICATION', 'Y:\OpenServer\domains\dobriruki.ua/admin/');
+define('DIR_SYSTEM', 'Y:\OpenServer\domains\dobriruki.ua/system/');
+define('DIR_LANGUAGE', 'Y:\OpenServer\domains\dobriruki.ua/admin/language/');
+define('DIR_TEMPLATE', 'Y:\OpenServer\domains\dobriruki.ua/admin/view/template/');
+define('DIR_CONFIG', 'Y:\OpenServer\domains\dobriruki.ua/system/config/');
+define('DIR_IMAGE', 'Y:\OpenServer\domains\dobriruki.ua/image/');
+define('DIR_CACHE', 'Y:\OpenServer\domains\dobriruki.ua/system/cache/');
+define('DIR_DOWNLOAD', 'Y:\OpenServer\domains\dobriruki.ua/system/download/');
+define('DIR_UPLOAD', 'Y:\OpenServer\domains\dobriruki.ua/system/upload/');
+define('DIR_LOGS', 'Y:\OpenServer\domains\dobriruki.ua/system/logs/');
+define('DIR_MODIFICATION', 'Y:\OpenServer\domains\dobriruki.ua/system/modification/');
+define('DIR_CATALOG', 'Y:\OpenServer\domains\dobriruki.ua/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dobriruki');
+define('DB_PASSWORD', 'dobriruki');
+define('DB_DATABASE', 'dobriruki');
+define('DB_PREFIX', 'oc_');
